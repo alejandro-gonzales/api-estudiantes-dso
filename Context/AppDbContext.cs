bee4eadafe39e0548bf12db49b0c781243780a32
alejandro-gonzales <alejandro.g280211@gmail.com>
2021-06-10 19:39:27 -0400

    Added context and controller person

---
+++ Context/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ApiEstudiantesV2.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiEstudiantesV2.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+        {
+
+        }
+        public DbSet<Persona> persona { get; set; }
+        public DbSet<Estudiante> estudiante { get; set; }
+        public DbSet<Carrera> carrera { get; set; }
+        public DbSet<Usuario> usuario { get; set; }
+        public DbSet<TipoUsuario> tipoUsuario { get; set; }
+    }
+    
+
+}
